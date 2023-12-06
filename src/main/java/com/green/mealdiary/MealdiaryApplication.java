e3dc9f4efdded3a4475d8642c680d94d6c7f17c0
oxoxooox248 <xoxoxxxo248@gmail.com>
2023-12-06 09:47:50 +0900

    프로젝트 생성

---
+++ src/main/java/com/green/mealdiary/MealdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.green.mealdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealdiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealdiaryApplication.class, args);
+    }
+
+}
