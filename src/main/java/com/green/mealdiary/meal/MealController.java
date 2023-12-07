25cf3c2d4364a9c6e0ceefc811b4007791f0083c
hana <ansgksk97@naver.com>
2023-12-07 16:44:29 +0900

    하나 - 컨트롤러 작업

--- src/main/java/com/green/mealdiary/meal/MealController.java
+++ src/main/java/com/green/mealdiary/meal/MealController.java
@@ -22,29 +22,54 @@ public class MealController {
     public ResVo postMeal(@RequestBody MealInsDto dto){
         return service.postMeal(dto);
     }
+
     @PostMapping("/api/meal/search")
     @Operation(summary = "일지 검색", description = "일지 검색 처리<br>(search: 검색내용)")
     public List<MealSelVo> searchMeal(String search){
         return null;
     }
+
     @GetMapping("/api/meal")
     @Operation(summary ="첫 화면", description = "첫 메인 화면 처리<br>(page: 페이지)")
     public List<MealSelVo> getMeal(int page){
         return null;
     }
+
     @GetMapping("/api/meal2")
     @Operation(summary ="페이징 처리", description = "메인 화면과 다른 페이징 처리<br>(page: 페이지)")
     public List<MealSelVo> getMeal2(int page){
         return null;
     }
+
     @GetMapping("/api/meal/book_mark")
     @Operation(summary ="책갈피 검색", description = "책갈피를 등록해놓은 일지 검색")
     public List<MealSelVo> getMealByBookMark(){
         return null;
     }
+
     @GetMapping("/api/meal/more")
     @Operation(summary ="일지의 상세 정보 페이지", description = "일지 클릭 시 나오는 상세 페이지 처리<br>(imeal: 일지 pk)")
     public MealSelMoreVo getMealMore(int imeal){
         return null;
     }
+
+    @PutMapping("/api/meal")
+    @Operation(summary = "일지 수정", description = "???")
+    public ResVo updMeal() { return null;}
+
+    @DeleteMapping("/api/meal")
+    @Operation(summary = "일지 삭제", description = "???")
+    public ResVo delMeal() { return null;}
+
+    @DeleteMapping("/api/meal/pic")
+    @Operation(summary = "일지 사진 삭제", description = "???")
+    public ResVo delMealPic() {return null;}
+
+    @GetMapping("/api/meal/mark")
+    @Operation(summary = "북마크 표시/해제", description = "???")
+    public ResVo toggleBookmark() {return null;}
+
+    @DeleteMapping("/api/meal/tag")
+    @Operation(summary = "일지 태그 삭제", description = "???")
+    public ResVo delMealTag() {return null;}
 }
