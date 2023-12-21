13fb6f173ec6fe8fd441ddd560b0e773f1f9c529
carpediem <luvrel@naver.com>
2023-12-21 16:06:06 +0900

    controller 수정(12/10)

---
+++ src/main/java/com/green/mealdiary/meal/model/MealPicUpdDto.java
@@ -0,0 +1,9 @@
+package com.green.mealdiary.meal.model;
+
+import lombok.Data;
+
+@Data
+public class MealPicUpdDto {
+    private int ipic;
+    private String pic;
+}
---
+++ src/main/java/com/green/mealdiary/meal/model/MealTagUpdDto.java
@@ -0,0 +1,9 @@
+package com.green.mealdiary.meal.model;
+
+import lombok.Data;
+
+@Data
+public class MealTagUpdDto {
+    private int itag;
+    private String tag;
+}
